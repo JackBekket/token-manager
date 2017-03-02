4168d8a015dbeef4957353ea50763c41bb673614
JackBekket <jackatwork@yandex.ru>
2017-03-02 18:33:29 +0300

    some fix

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -74,6 +74,8 @@ window.App = {
       accounts = accs;
       account = accounts[0];
 
+
+        //Need(!!) to remove it when deploying live
         $("#transfer_to").val(accounts[1]);
         console.log("accounts1");
         console.log(accounts[1]);
@@ -505,7 +507,7 @@ function insSuccess(data) {
 window.addEventListener('load', function() {
   // Checking if Web3 has been injected by the browser (Mist/MetaMask)
   if (typeof web3 !== 'undefined') {
-    console.warn("Using web3 detected from external source. If you find that your accounts don't appear or you have 0 MetaCoin, ensure you've configured that source properly. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-metamask")
+    console.warn("Using web3 detected from external source. If you find that your accounts don't appear or you have 0 Coin, ensure you've configured that source properly. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-metamask")
     // Use Mist/MetaMask's provider
     window.web3 = new Web3(web3.currentProvider);
   } else {
